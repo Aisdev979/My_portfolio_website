aa95531943466c48101294cd5cfc0d121b9f3c8a
Aisdev <osarugueogbebor@gmail.com>
2023-10-22 13:36:32 +0100

    added project and form section. And did responsive mode for for tablet and desktop.

--- main.js
+++ main.js
@@ -1,10 +1,43 @@
 const hamburgerMenu = document.querySelector('.nav__menu');
-const nav = document.querySelector('.nav__link');
+const closeMenu = document.querySelector('.nav__link');
+const userName = document.querySelector('#Name');
+const userEmail = document.querySelector('#Email');
+const submitBtn = document.querySelector('#submit');
+const nameErrorMessage = document.querySelector('#error-name');
+const enailErrorMessage = document.querySelector('#error-email');
 
 hamburgerMenu.addEventListener("click", () => {
-    nav.style.left = "15em"
+    closeMenu.style.left = "10em"
 });
 
-nav.addEventListener("click", () => {
-    nav.style.left = "100em";
+closeMenu.addEventListener("click", () => {
+    closeMenu.style.left = "70em";
+});
+
+function isValidName() {
+    if (userName.value.trim() === "") {
+        nameErrorMessage.textContent = `Name field can't be empty`;
+    } else {
+        nameErrorMessage.textContent = "";
+    }
+}
+
+function validEmail(userEmail) {
+    return /^(([^<>()[\]\\.,;:\$@"]+(\.[^<>()[\]\\.,;:\$@"]+)*)!(".+"))@((\[[0-9]{1,3}\.[0-9{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])!(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(userEmail);
+}
+
+function isValidEmail() {
+    if (userEmail.value.trim() === "") {
+        enailErrorMessage.textContent = `Email field can't be empty`;
+    } else if (!validEmail(userEmail.value.trim())) {
+        enailErrorMessage.textContent   = `Not a valid Email`;
+    } else {
+        enailErrorMessage.textContent = "";
+    }
+}
+
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    isValidName();
+    isValidEmail();
 })

30e9e83089d7166668f91cf1158287f496ff7716
Aisdev <osarugueogbebor@gmail.com>
2023-10-26 05:44:32 +0100

    change the form layout

--- main.js
+++ main.js
@@ -14,30 +14,51 @@ closeMenu.addEventListener("click", () => {
     closeMenu.style.left = "70em";
 });
 
-function isValidName() {
-    if (userName.value.trim() === "") {
-        nameErrorMessage.textContent = `Name field can't be empty`;
-    } else {
-        nameErrorMessage.textContent = "";
-    }
-}
+// function isValidName() {
+//     if (userName.value.trim() === "") {
+//         nameErrorMessage.textContent = `Name field can't be empty`;
+//     } else {
+//         nameErrorMessage.textContent = "";
+//     }
+// }
 
-function validEmail(userEmail) {
-    return /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/.test(userEmail);
-}
+// function validEmail(userEmail) {
+//     return /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/.test(userEmail);
+// }
 
-function isValidEmail() {
-    if (userEmail.value.trim() === "") {
-        enailErrorMessage.textContent = `Email field can't be empty`;
-    } else if (validEmail(userEmail.value.trim())) {
-        enailErrorMessage.textContent = "";
-    } else {
-        enailErrorMessage.textContent   = `Not a valid Email`;
-    }
-}
+// function isValidEmail() {
+//     if (userEmail.value.trim() === "") {
+//         enailErrorMessage.textContent = `Email field can't be empty`;
+//     } else if (validEmail(userEmail.value.trim())) {
+//         enailErrorMessage.textContent = "";
+//     } else {
+//         enailErrorMessage.textContent   = `Not a valid Email`;
+//     }
+// }
 
-submitBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    isValidName();
-    isValidEmail();
-})
+// submitBtn.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     isValidName();
+//     isValidEmail();
+// })
+
+
+// submitBtn.addEventListener("submit", (e) => {
+//     e.preventDefault()
+
+//     fetch("https://formsubmit.co/el/lehuta", {
+//         method: post,
+//         body: new FormData(this),
+//     })
+//     .then(response => {
+//         console.log(response);
+//         if(response.ok) {
+//             window.location.href = "thanks.html";
+//         } else {
+//             console.error("Fail to submit form");
+//         }
+//     })
+//     .catch(error => {
+//         console.error(`"Error:" error`);
+//     })
+// })
